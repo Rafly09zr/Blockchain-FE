c02b3e623089c105d45191fa462d48f3fb19653a
Rafly09zr <rafly.zaki2902@mail.ugm.ac.id>
2023-12-20 15:26:38 +0700

    Update: changing the UI

--- app/landing/page.js
+++ app/landing/page.js
@@ -7,7 +7,7 @@ import Image from 'next/image';
 
 const Landing = () => {
   return (
-    <div className="bg-neutral-100 w-full h-screen mx-auto flex justify-center items-center">
+    <div className="bg-image w-full h-screen mx-auto flex justify-center items-center">
       {/* <Navbar /> */}
       <div className="text-center ml-[148px] mr-[148px]">
         <h1 className="text-6xl pt-[60px] mx-auto text-neutral-700">Agriculture Supply Chain</h1>
@@ -15,7 +15,7 @@ const Landing = () => {
         <p className="max-w-[599px] min-h-[100px] text-base pt-[16px] pb-[16px] mx-auto text-neutral-600 mt-4">We utilize blockchain to enhance transparency in agricultural product distribution. Track each product’s journey from origin to destination securely and reliably with blockchain technology.</p>
         <Button>Get Started</Button>
         <div className="pt-[60px]">
-          <Image src="/images/illus-landing.png" width={1144} height={288} alt="404 text" />
+          <Image src="/images/illus-landing2.png" width={1144} height={288} alt="404 text" />
         </div>
         {/* <img src={imageSrc} alt="Deskripsi Gambar" className="mt-8 max-w-full" /> */}
       </div>
