13c7bc4090144186a466744ed380882e66a9ca51
Rafly09zr <rafly.zaki2902@mail.ugm.ac.id>
2023-12-21 10:57:22 +0700

    Add: fetching address from metamask

---
+++ app/MetaMaskService.js
@@ -0,0 +1,20 @@
+// MetaMaskService.js
+const fetchMetaMaskAddress = async () => {
+    if (window.ethereum) {
+      try {
+        const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+        if (accounts.length > 0) {
+          return accounts[0]; // Alamat dari MetaMask
+        }
+      } catch (error) {
+        console.error('Error fetching MetaMask address:', error);
+        return null;
+      }
+    } else {
+      console.log('MetaMask not installed');
+      return null;
+    }
+  };
+  
+  export default fetchMetaMaskAddress;
+  
